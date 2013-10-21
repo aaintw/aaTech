f795b9b91bf99190d3ac43c62552c4834df91c1e
Shu Cheng <cs90513@gmail.com>
2013-10-21 15:37:02 -0400

    ClimbStrair with empty body

---
+++ ClimbStairs.java
@@ -0,0 +1,25 @@
+
+import java.util.HashMap;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AA
+ */
+public class ClimbStairs {
+        HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+    public int climbStairs(int n) {
+        // if(n == 0 || n ==1){
+        //     return 1;
+        // }        
+        // if(hm.containsKey(n)){
+        //     return hm.get(n);
+        // }
+        // int num = climbStairs(n-1)+climbStairs(n-2);
+        // return num;
+    }
+}
